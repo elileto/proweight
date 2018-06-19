db3d92500158ac68a15d21734f6fb6e7e115df05
Elizabeth Letourneau <elizabeth.letourneau@shopify.com>
2018-06-19 15:04:06 -0400

    adding polaris

--- app/components/GameItem.js
+++ app/components/GameItem.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import {Button} from '@shopify/polaris';
 
 export default function GameItem({onAddGame, game: {name}}) {
   return (
     <li>
       <p>{name}</p>
+      <Button> 
+        Thar she blows
+      </Button>
       <button
         onClick={() => {
           onAddGame(name);
