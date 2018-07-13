fd3267c32a0d6b12d9973a1eeac00ee1d84df508
Elizabeth Letourneau <elizabeth.letourneau@shopify.com>
2018-07-13 14:29:36 -0400

    fixed add product query

--- app/routes/Settings.js
+++ app/routes/Settings.js
@@ -1,18 +1,31 @@
 import React from 'react';
-import {Page} from '@shopify/polaris';
+import {Page, Card, Form, TextField, FormLayout, Button, Banner} from '@shopify/polaris';
 import {ApolloProvider} from 'react-apollo';
 import ApolloClient, {gql} from 'apollo-boost';
 import Fetch from 'react-fetch-component';
-import {Mutation} from 'react-apollo';
+import {Mutation, Query} from 'react-apollo';
 import RoutePropagator from '@shopify/react-shopify-app-route-propagator';
-import {Switch, Route, withRouter} from 'react-router';
-import GameList from '../components/GameList';
+import { withRouter} from 'react-router';
+
 
 const client = new ApolloClient({
   fetchOptions: {
     credentials: 'include',
   },
 });
+
+const FIND_COLLECTION = gql`
+{
+  collections(first: 1, query: "title:'weighted'") {
+    edges {
+      node {
+        title
+        id
+      }
+    }
+  }
+}
+`;
 
 const CREATE_PRODUCT = gql`
   mutation CreateProduct($product: ProductInput!) {
@@ -27,66 +40,113 @@ const CREATE_PRODUCT = gql`
 
 const Propagator = withRouter(RoutePropagator);
 
+class AddProduct extends React.Component
+    { 
 
-    export default function Dashboard() { 
+      state = {
+        title: '',
+        description: '',
+      };
+
+      
+      
+      render(){
+
+      const {title, description} = this.state;
+
+      function mutate(createProduct) {
+        const productInput = {
+          title: title,
+          bodyHtml: description
+        };
+  
+        createProduct({
+          variables: {
+            product: productInput
+          }
+        });
+  
+      }
+
       return (
           <Page
-            title="Create Your Product">
+            title=" Add  a Product to your Weighted Collection">
               <ApolloProvider client={client}>
-              <Fetch url="https://boardgameslist.herokuapp.com" as="json">
-                  {(fetchResults) => {
-                    if (fetchResults.loading) {
-                      return <p>Loading</p>
-                    }
-                  
-                    if (fetchResults.error) {
-                      return <p>failed to fetch games</p>
-                    }
-                  
+
+              <Query query={FIND_COLLECTION} >
+                {({ loading, error, data }) => {
+                 if (loading) return null;
+                if (error) return `Error!: ${error}`;
+              
                     return (<Mutation mutation={CREATE_PRODUCT}>
                       {(createProduct, mutationResults) => {
-                        const loading = mutationResults.loading && <p>loading... </p>;
-                      
-                        const error = mutationResults.error && <p>error creating product</p>;
-                      
-                        const success = mutationResults.data && (
-                          <p>
-                            successfully created &nbsp;
-                            {mutationResults.data.productCreate.product.title}
-                          </p>
-                        );
-                      
+                          const loading = mutationResults.loading && <Banner title="Loading...">
+                          <p>Creating product</p>
+                          </Banner>;
+
+                          const error = mutationResults.error && <Banner title="Error" status="warning">
+                          <p>Product could not be created</p>
+                          </Banner>;
+
+                          const success = mutationResults.data && (<Banner title="Success" status="success">
+                          <p>Successfully created</p>
+                          </Banner>);
+
                         return (
-                          <React.Fragment>
-                            <Propagator />
-                            <GameList
-                              games={fetchResults.data}
-                              onAddGame={(title) => {
-                                const productInput = {
-                                  title: title,
-                                  productType: 'board game',
-                                };
-                              
-                                createProduct({
-                                  variables: {product: productInput},
-                                });
-                              }}
-                            />
-                            {loading}
-                            {error}
-                            {success}
-                          </React.Fragment>
-                        );
+                          <Card>
+                          <Form onSubmit= {(event) => {
+                            this.setState({title: {title}}, {description: {description}});
+                            mutate(createProduct);
+                            }}>
+                          <Card.Section>
+                          {loading}
+                          {error}
+                          {success}
+                            <FormLayout>
+                            <TextField
+                                value={title}
+                                onChange={this.handleChange('title')}
+                                label="Title"
+                                type="text"
+                                helpText={
+                                  <span>
+                                    This will be the name of your product.
+                                  </span>
+                                }
+                              />
+                              <TextField
+                                value={description}
+                                onChange={this.handleChange('description')}
+                                label="Description"
+                                type="text"
+                                helpText={
+                                  <span>
+                                    This will be the description of your product.
+                                  </span>
+                                }
+                              />
+
+                              <Button submit>Submit</Button>
+                            </FormLayout>
+                            </Card.Section>
+                          </Form>
+                          </Card>      
+                          )
                       }}
                     </Mutation>)
                   }}
-
-                </Fetch>
+                </Query>
               </ApolloProvider>
           </Page>    
       );
     }
 
+    handleChange = (field) => {
+      return (value) => this.setState({[field]: value});
+    };
 
+  }
+
+    export default  AddProduct;
   
   
