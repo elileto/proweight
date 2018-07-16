f476e346d474982a7f0dd171ef0039a54e86296c
Elizabeth Letourneau <elizabeth.letourneau@shopify.com>
2018-07-16 09:59:45 -0400

    added mutation, fixing images

--- app/routes/Settings.js
+++ app/routes/Settings.js
@@ -1,12 +1,7 @@
 import React from 'react';
-import {Page, Card, Form, TextField, FormLayout, Button, Banner} from '@shopify/polaris';
-import {ApolloProvider} from 'react-apollo';
+import {Page, Card, Form, TextField, FormLayout, Button, Banner, Stack, Thumbnail, DropZone, Caption } from '@shopify/polaris';
+import {ApolloProvider, Mutation, Query} from 'react-apollo';
 import ApolloClient, {gql} from 'apollo-boost';
-import Fetch from 'react-fetch-component';
-import {Mutation, Query} from 'react-apollo';
-import RoutePropagator from '@shopify/react-shopify-app-route-propagator';
-import { withRouter} from 'react-router';
-
 
 const client = new ApolloClient({
   fetchOptions: {
@@ -38,7 +33,7 @@ const CREATE_PRODUCT = gql`
   }
 `;
 
-const Propagator = withRouter(RoutePropagator);
+
 
 class AddProduct extends React.Component
     { 
@@ -46,27 +41,61 @@ class AddProduct extends React.Component
       state = {
         title: '',
         description: '',
+        files: [],
       };
 
       
       
       render(){
 
-      const {title, description} = this.state;
+      const {title, description, files} = this.state;
+
+      
 
       function mutate(createProduct) {
+        let images;
+
+        {files.map((file) => (
+          images = window.URL.createObjectURL(file)
+          ))} 
         const productInput = {
           title: title,
-          bodyHtml: description
+          bodyHtml: description,
+          images: {src: images}, 
+          collectionsToJoin: ["gid://shopify/Collection/57963610168"]
+
         };
-  
-        createProduct({
-          variables: {
-            product: productInput
-          }
-        });
+
+      createProduct({
+        variables: {
+          product: productInput
+        }
+      });
   
       }
+      const validImageTypes = ['image/gif', 'image/jpeg', 'image/png'];
+      
+      const fileUpload = !files.length && <DropZone.FileUpload />;
+      const uploadedFiles = files.length > 0 && (
+        <Stack vertical>
+          {files.map((file) => (
+            <Stack alignment="center">
+              <Thumbnail
+                size="small"
+                alt={file.name}
+                source={
+                  validImageTypes.indexOf(file.type) > 0
+                    ? window.URL.createObjectURL(file)
+                    : 'https://cdn.shopify.com/s/files/1/0757/9955/files/New_Post.png?12678548500147524304'
+                }
+              />
+              <div>
+                {file.name} <Caption>{file.size} bytes</Caption>
+              </div>
+            </Stack>
+          ))}
+        </Stack>
+        );
 
       return (
           <Page
@@ -77,7 +106,7 @@ class AddProduct extends React.Component
                 {({ loading, error, data }) => {
                  if (loading) return null;
                 if (error) return `Error!: ${error}`;
-              
+
                     return (<Mutation mutation={CREATE_PRODUCT}>
                       {(createProduct, mutationResults) => {
                           const loading = mutationResults.loading && <Banner title="Loading...">
@@ -90,46 +119,58 @@ class AddProduct extends React.Component
 
                           const success = mutationResults.data && (<Banner title="Success" status="success">
                           <p>Successfully created</p>
+                          
                           </Banner>);
 
                         return (
                           <Card>
-                          <Form onSubmit= {(event) => {
-                            this.setState({title: {title}}, {description: {description}});
-                            mutate(createProduct);
-                            }}>
-                          <Card.Section>
-                          {loading}
-                          {error}
-                          {success}
-                            <FormLayout>
-                            <TextField
-                                value={title}
-                                onChange={this.handleChange('title')}
-                                label="Title"
-                                type="text"
-                                helpText={
-                                  <span>
-                                    This will be the name of your product.
-                                  </span>
-                                }
-                              />
-                              <TextField
-                                value={description}
-                                onChange={this.handleChange('description')}
-                                label="Description"
-                                type="text"
-                                helpText={
-                                  <span>
-                                    This will be the description of your product.
-                                  </span>
-                                }
-                              />
+                            
+                            <Form onSubmit= {(event) => {
+                              this.setState({title: {title}}, {description: {description}, files: {files}});
+                              mutate(createProduct);
+                              }}>
+                              <Card.Section>
+                              {loading}
+                              {error}
+                              {success}
+                                <FormLayout>
+                                
+                                  <TextField
+                                      value={title}
+                                      onChange={this.handleChange('title')}
+                                      label="Title"
+                                      type="text"
+                                      helpText={
+                                        <span>
+                                          This will be the name of your product.
+                                        </span>
+                                      }
+                                  />
+                                  <TextField
+                                    value={description}
+                                    onChange={this.handleChange('description')}
+                                    label="Description"
+                                    type="text"
+                                    helpText={
+                                      <span>
+                                        This will be the description of your product.
+                                      </span>
+                                    }
+                                  />
+                                  <DropZone
+                                    onDrop={(files) => {
+                                      this.setState({files: [...this.state.files, ...files]});
+                                    }}
+                                    type="image"
+                                  >
+                                    {uploadedFiles}
+                                    {fileUpload}
+                                  </DropZone>
 
-                              <Button submit>Submit</Button>
-                            </FormLayout>
-                            </Card.Section>
-                          </Form>
+                                  <Button submit>Submit</Button>
+                                </FormLayout>
+                              </Card.Section>
+                            </Form>
                           </Card>      
                           )
                       }}
@@ -146,7 +187,7 @@ class AddProduct extends React.Component
     };
 
   }
-
-    export default  AddProduct;
+  
+  export default  AddProduct;
   
   
